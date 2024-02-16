9be0ecd1f2b66996abd528b138b6b121f7f0ef74
DevisonSantana <devisonsantana@hotmail.com>
2024-02-16 01:15:19 -0300

    create singleton proj in java

---
+++ src/padroessingleton/Test.java
@@ -0,0 +1,24 @@
+package padroessingleton;
+import SingletonLazy;
+import SingletonEager;
+import SingletonLazyHolder;
+
+public class Test {
+    public static void main(String[] args) {
+        // TESTES DE SINGLETON
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder holder = SingletonLazyHolder.getInstancia();
+        System.out.println(holder);
+        holder = SingletonLazyHolder.getInstancia();
+        System.out.println(holder);
+    }
+}
